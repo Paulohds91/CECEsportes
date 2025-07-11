e6462bda232cffb0b0608e026532795d6a002405
Paulo Henrique da Silva <paulo04121991@gmail.com>
2025-07-11 13:15:58 -0300

    Delete models/Checkin.js

--- models/Checkin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const CheckinSchema = new mongoose.Schema({
-  aluno: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  data_hora: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Checkin', CheckinSchema); 
